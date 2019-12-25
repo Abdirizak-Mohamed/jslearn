c9aec5f7fce03d6d91046b8dda75797810185799
Abdirizak Mohamed <amohamed5@Abdirizaks-MacBook-Air.local>
2019-12-25 13:24:00 +0300

    Initial sidebar

--- jslearn/src/App.js
+++ jslearn/src/App.js
@@ -4,6 +4,9 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 
 function App() {
@@ -14,6 +17,24 @@ function App() {
           <Navbar.Brand href="#">Navbar</Navbar.Brand>
         </Container>
       </Navbar>
+      <Container>
+       <Row>
+        <Col sm={4}>
+        <ListGroup>
+         <ListGroup.Item>No style</ListGroup.Item>
+         <ListGroup.Item variant="primary">Primary</ListGroup.Item>
+         <ListGroup.Item variant="secondary">Secondary</ListGroup.Item>
+         <ListGroup.Item variant="success">Success</ListGroup.Item>
+         <ListGroup.Item variant="danger">Danger</ListGroup.Item>
+         <ListGroup.Item variant="warning">Warning</ListGroup.Item>
+         <ListGroup.Item variant="info">Info</ListGroup.Item>
+         <ListGroup.Item variant="light">Light</ListGroup.Item>
+         <ListGroup.Item variant="dark">Dark</ListGroup.Item>
+        </ListGroup>
+       </Col>
+        <Col sm={8}>sm=4</Col>
+       </Row>
+       </Container>
      </React.Fragment> 
   );
 }
