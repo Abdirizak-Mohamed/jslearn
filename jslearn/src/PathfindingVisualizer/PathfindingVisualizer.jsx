7a73bc4ef1fc9dba9fab85e6ee73e2e70270d42c
Abdirizak Mohamed <amohamed5@Abdirizaks-MacBook-Air.local>
2020-01-04 15:58:30 +0300

    Reset button Kinda working

--- jslearn/src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ jslearn/src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -86,6 +86,7 @@ export default class PathfindingVisualizer extends Component {
       const node = nodes[i];
       document.getElementById(`node-${node.row}-${node.col}`).className =
         "node";
+      this.checkIsWall(node);
     }
     console.log(nodes[0]);
 
@@ -93,6 +94,12 @@ export default class PathfindingVisualizer extends Component {
         "node node-shortest-path";*/
 
     this.setState({ grid });
+  };
+
+  checkIsWall = node => {
+    if (node.isWall === true) {
+      return node.isWall === false;
+    }
   };
 
   getAllNodes = grid => {
@@ -148,6 +155,7 @@ export default class PathfindingVisualizer extends Component {
     );
   }
 }
+
 const getInitialGrid = () => {
   const grid = [];
   for (let row = 0; row < 20; row++) {
